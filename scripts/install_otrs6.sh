0125d2675e9ac90db7fbc080be83e9b4152666bb
amaurybsouza <amaurybsouza@gmail.com>
2019-09-23 15:59:57 -0300

    new module

--- scripts/install_otrs6.sh
+++ scripts/install_otrs6.sh
@@ -1,6 +1,17 @@
 #! /usr/bin/env bash
+# ------------------------------------------------------------------------ #
+# Script Name:   install_otrs6.sh
+# Description:   Show informations about machine hardware.
+# Written by:    Amaury Souza
+# Maintenance:   Amaury Souza
+# ------------------------------------------------------------------------ #
+# Usage:
+#       $ ./install_otrs6.sh
+# ------------------------------------------------------------------------ #
+# Bash Version:
+#              Bash 4.4.19
+# ------------------------------------------------------------------------ #
 
-#Not execute this script yet, it not termined!
 
 #Starting script
 
@@ -11,23 +22,17 @@ yum install net-tools vim epel-release -y
 
 sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 
-echo In this section you will to reboot your system. After it, you can to next!
-
-sudo shutdown -r now
-
 #Install MariaDB Server and Apache Web Server
 
 yum install httpd httpd-devel gcc mariadb-server -y
 systemctl start httpd
 systemctl enable httpd
-
 echo 
 "max_allowed_packet=64M
 query_cache_size=32M
 innodb_log_file_size=256M
 character-set-server=utf8
 collation-server=utf8_unicode_ci" >> /etc/my.cnf 
-
 systemctl start mariadb
 systemctl enable mariadb
 mysql_secure_installation
